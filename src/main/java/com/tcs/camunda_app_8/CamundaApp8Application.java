598a3defb544a66b62eed72ce5fdf22383607483
imteyazkhan374 <imteyazkhan374@gmail.com>
2025-07-22 17:13:22 +0530

    no message

--- src/main/java/com/tcs/camunda_app_8/CamundaApp8Application.java
+++ src/main/java/com/tcs/camunda_app_8/CamundaApp8Application.java
@@ -2,21 +2,11 @@ package com.tcs.camunda_app_8;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import io.camunda.zeebe.client.ZeebeClient;
 
 @SpringBootApplication
 public class CamundaApp8Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CamundaApp8Application.class, args);
-	}
-
-	@Bean
-	ZeebeClient zeebeClient() {
-		return ZeebeClient.newClientBuilder().gatewayAddress("127.0.0.1:26500") // Must match application.yml
-				.usePlaintext() // Critical - must match Zeebe broker config
-				.build();
 	}
 }
